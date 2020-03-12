6402f9583b67bb66a28b177c26f6ac98ebfc598b
Hiromu OCHIAI <otiai10@gmail.com>
2020-03-12 21:24:57 +0200

    Refactor

--- Sources/SwiftExampleCommand/main.swift
+++ Sources/SwiftExampleCommand/main.swift
@@ -6,10 +6,16 @@ let fpath = CommandLine.arguments[1]
 print("[0001] ARG PATH:", fpath)
 
 let contents = FileManager.default.contents(atPath: fpath)!
-
 let plist = try PropertyListSerialization.propertyList(from: contents, options: .mutableContainersAndLeaves, format: nil) as! NSMutableDictionary
 
-plist["CFBundleVersion"] = "3"
+let propKey = "CFBundleVersion"
+plist[propKey] = {(currentVersion: Any?) -> Int in
+    if let v: Int = currentVersion as? Int {
+        return v + 1
+    }
+    return 1
+}(plist[propKey])
+
 
 if let data = try? PropertyListSerialization.data(fromPropertyList: plist, format: .xml, options: .max) {
     try data.write(to: NSURL(fileURLWithPath: fpath) as URL)
